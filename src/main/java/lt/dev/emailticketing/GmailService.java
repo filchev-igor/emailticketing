3334c53f2bbfe6ec1300ad5dd064590406dfa56c
Igor Filchev <filchev.igor@gmail.com>
2025-03-08 21:02:27 +0200

    Mail solution

--- src/main/java/lt/dev/emailticketing/GmailService.java
+++ src/main/java/lt/dev/emailticketing/GmailService.java
@@ -1,14 +1,19 @@
 package lt.dev.emailticketing;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.gmail.Gmail;
 import com.google.api.services.gmail.model.Message;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.auth.http.HttpCredentialsAdapter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -17,7 +22,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
+import java.io.File;
+import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.List;
 
@@ -26,6 +32,7 @@ public class GmailService {
     private Gmail gmail;
     private final RestTemplate restTemplate = new RestTemplate();
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String TOKENS_DIRECTORY_PATH = "tokens"; // Stores tokens in project dir
 
     @Value("${apex.rest.endpoint}")
     private String apexEndpoint;
@@ -33,9 +40,23 @@ public class GmailService {
     @PostConstruct
     public void init() throws Exception {
         NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-        GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream("src/main/resources/credentials.json"))
-                .createScoped(Collections.singleton("https://www.googleapis.com/auth/gmail.modify"));
-        gmail = new Gmail.Builder(httpTransport, JSON_FACTORY, new HttpCredentialsAdapter(credentials))
+        // Load client secrets from credentials.json
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
+                new InputStreamReader(new ClassPathResource("credentials.json").getInputStream()));
+
+        // Build OAuth 2.0 flow and trigger user authorization
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                httpTransport, JSON_FACTORY, clientSecrets,
+                Collections.singleton("https://www.googleapis.com/auth/gmail.modify"))
+                .setDataStoreFactory(new FileDataStoreFactory(new File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+
+        // Build Gmail service
+        gmail = new Gmail.Builder(httpTransport, JSON_FACTORY, credential)
                 .setApplicationName("EmailTicketing")
                 .build();
     }
