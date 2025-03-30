59fa6cac476e41502f9f0b92cc35501373e5350c
Igor Filchev <filchev.igor@gmail.com>
2025-03-30 18:58:17 +0300

    Add reply controller

--- src/test/java/lt/dev/emailticketing/controller/ReplyControllerTest.java
+++ src/test/java/lt/dev/emailticketing/controller/ReplyControllerTest.java
@@ -5,8 +5,8 @@ import lt.dev.emailticketing.service.GmailService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.*;
@@ -15,14 +15,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.springframework.beans.factory.annotation.Value;
 
-@SuppressWarnings("removal")
 @WebMvcTest(ReplyController.class)
 class ReplyControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private GmailService gmailService;
 
     @Value("${apex.api.key}")
